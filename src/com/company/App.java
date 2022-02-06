57ce754f13c4a08bcf94a918f1e9f0a532844542
SlavaBasov <slavabasov987@gmail.com>
2022-02-06 16:31:13 +0300

    added array

--- src/com/company/App.java
+++ src/com/company/App.java
@@ -3,6 +3,10 @@ package com.company;
 public class App {
 
     public static void main(String[] args) {
+        int[][] array = {{1,3,3},
+                         {2,1,4},
+                         {0,6,4}};
 
+        
     }
 }

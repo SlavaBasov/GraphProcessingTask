78dd3f87e7e2e6325f91a4e0c84e92a82081c72b
SlavaBasov <slavabasov987@gmail.com>
2022-02-06 16:35:08 +0300

    added ArrayHandler class

---
+++ src/com/company/ArrayHandler.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Queue;
+
+public class ArrayHandler {
+    private int dist[][];
+    private int i;
+    private int j;
+    private int V;
+    private int[][] array;
+}

768b8407fafa2d424028892ebf671f1da2cfaed3
SlavaBasov <slavabasov987@gmail.com>
2022-02-06 17:43:43 +0300

    adding longestWayst[][] and longestPath fields, dijkstra and processNeighbours methods

--- src/com/company/ArrayHandler.java
+++ src/com/company/ArrayHandler.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ArrayHandler {
@@ -7,4 +8,63 @@ public class ArrayHandler {
     private Queue<Node> pq;
     private int V;
     private int[][] array;
+    private int longestWays[][];
+    private Node longestPath;
+
+    public ArrayHandler(int[][] array) {
+        this.V = array.length;
+        dist = new int[V][V];
+        pq = new PriorityQueue<>(V * V, new Node());
+        longestWays=new int[V][V];
+        longestPath = new Node(0,0,0);
+    }
+
+    public void dijkstra(int[][] array, int iSrc, int jSrc) {
+        this.array = array;
+
+        pq.add(new Node(iSrc, jSrc, array[iSrc][jSrc]));
+
+        dist[iSrc][jSrc] = array[iSrc][jSrc];
+        while (true) {
+            if (pq.isEmpty()) {
+                if(longestWays[iSrc][jSrc]>longestPath.getCost())
+                    longestPath.setI(iSrc);
+                longestPath.setJ(jSrc);
+                longestPath.setCost(longestWays[iSrc][jSrc]);
+                return;
+            }
+            int i = pq.element().getI();
+            int j = pq.element().getJ();
+            pq.remove();
+
+            if(j!=0)
+                processNeighbours(i, j);
+
+            if(j==0 && dist[i][j]>longestWays[iSrc][jSrc])
+                longestWays[iSrc][jSrc]=dist[i][j];
+        }
+
+    }
+
+    private void processNeighbours(int i, int j) {
+
+        pq.add(new Node(i, j-1, array[i][j-1]));
+        if(dist[i][j-1] < (dist[i][j]+array[i][j-1])){
+            dist[i][j-1]=dist[i][j]+array[i][j-1];
+        }
+
+        if(i!=dist.length - 1){
+            pq.add(new Node(i+1, j-1, array[i+1][j-1]));
+            if(dist[i+1][j-1]< (dist[i][j]+array[i+1][j-1])) {
+                dist[i+1][j-1]=dist[i][j]+array[i+1][j-1];
+            }
+        }
+
+        if(i!=0) {
+            pq.add(new Node(i - 1, j - 1, array[i - 1][j - 1]));
+            if(dist[i-1][j-1]< (dist[i][j]+array[i-1][j-1])) {
+                dist[i-1][j-1]=dist[i][j]+array[i-1][j-1];
+            }
+        }
+    }
 }
